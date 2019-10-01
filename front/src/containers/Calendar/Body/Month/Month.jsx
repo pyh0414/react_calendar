a5268d780da4e6478a2d66e7399280b101fe1b8f
yeonho Park <whwlsvy12@naver.com>
2019-10-01 16:50:47 +0900

    add week day

--- front/src/containers/Calendar/Body/Month/Month.jsx
+++ front/src/containers/Calendar/Body/Month/Month.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+import { WeekDayRow } from "./style";
+import WeekDayCol from "../../../../components/Calendar/Body/Month/WeekDay";
+
 const Month = () => {
-  return <div></div>;
+  const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+
+  const weekDay = dayOfWeek.map((v, i) => {
+    return <WeekDayCol key={i} day={v} />;
+  });
+
+  return (
+    <div>
+      <WeekDayRow>{weekDay}</WeekDayRow>
+    </div>
+  );
 };
+
+export default Month;
--- front/src/containers/Calendar/Body/Month/style.js
+++ front/src/containers/Calendar/Body/Month/style.js
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+
+export const WeekDayRow = styled.div`
+  display: flex;
+  text-align: center;
+  padding: 0.7em;
+  border-bottom: 1px solid #f2f2f2;
+  font-weight: 600;
+`;
