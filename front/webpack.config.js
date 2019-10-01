84868137733c334d4c8eb92164fd4c55ef27235c
yeonho Park <whwlsvy12@naver.com>
2019-10-01 16:09:27 +0900

    add css-loader

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
             "react-hot-loader/babel"
           ]
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
