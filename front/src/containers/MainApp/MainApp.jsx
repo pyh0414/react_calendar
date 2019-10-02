9c29faaa49edf2833ff2b95debaac2df9fcf0689
yeonho Park <whwlsvy12@naver.com>
2019-10-02 16:04:06 +0900

    initialize currentDate

--- front/src/containers/MainApp/MainApp.jsx
+++ front/src/containers/MainApp/MainApp.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import { Wrapper, Calendar } from "./style.js";
 
 import Head from "../Calendar/Head";
 import Body from "../Calendar/Body";
 
+import { INITIALIZE_CURRENT_DATE } from "../../../reducer/schedule";
+
 const MainAp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // 마운트 될때만 실행
+    dispatch({
+      type: INITIALIZE_CURRENT_DATE,
+      data: new Date()
+    });
+  }, []);
+
   return (
     <Wrapper>
       <Calendar>
