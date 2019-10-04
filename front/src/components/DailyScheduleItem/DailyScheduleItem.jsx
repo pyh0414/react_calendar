1a10a1b2ab39f1478abc53751158d0af38d7e33f
yeonho Park <whwlsvy12@naver.com>
2019-10-04 13:20:18 +0900

    add 하루종일 옵션

--- front/src/components/DailyScheduleItem/DailyScheduleItem.jsx
+++ front/src/components/DailyScheduleItem/DailyScheduleItem.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Popover } from "antd";
+import { useSelector } from "react-redux";
 
 import { Item } from "./style";
 
 const DailyScheduleItem = ({ data }) => {
   const { start, end, title, content, colorCode, id } = data;
+
+  const { mode } = useSelector(state => state.schedules);
+  const isMultipleDay =
+    mode === "day" && new Date(end).getDate() - new Date(start).getDate() > 0;
 
   const info = (
     <div>
@@ -23,7 +28,11 @@ const DailyScheduleItem = ({ data }) => {
   return (
     <div>
       <Popover content={info} trigger="hover" key={id}>
-        <Item colorCode={colorCode}></Item>
+        <Item colorCode={colorCode}>
+          {isMultipleDay && (
+            <span style={{ color: "white", marginLeft: "5px" }}>하루 종일</span>
+          )}
+        </Item>
       </Popover>
     </div>
   );
