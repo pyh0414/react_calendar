c5fb3e79a1f0b758d8fd87dae3cc57ed90d4ad82
yeonho Park <whwlsvy12@naver.com>
2019-10-04 15:09:00 +0900

    add server app.js bin/www

--- server/app.js
+++ server/app.js
@@ -23,7 +23,6 @@ app.use(
   })
 );
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -41,7 +40,6 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
